e165cd075052767bf9fafe53cc60a6a0027fee07
Gavin Zhanggit config --global user.email gwxzhang@bu.edu <gwxzhang@bu.edu>
2024-09-23 22:44:34 -0400

    Redirect root URL to quotes page

--- cs412/urls.py
+++ cs412/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path,include ## New Import
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hw/', include('hw.urls')),
-    path('quotes/', include('quotes.urls'))
+    path('quotes/', include('quotes.urls')),
+    path('', RedirectView.as_view(url='/quotes/'))  # Redirect root to /quotes/
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
