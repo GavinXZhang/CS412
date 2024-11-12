37b84190460fb778b171f6dc99ce7e529f37edd6
Gavin Zhanggit config --global user.email gwxzhang@bu.edu <gwxzhang@bu.edu>
2024-11-12 17:32:56 -0500

    Voter_Pk

--- voter_analytics/urls.py
+++ voter_analytics/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
 path(r"", views.ResultsListView.as_view(), name= "home"),
 path(r"results", views.ResultsListView.as_view(), name= "results_list"),
-path(r"result/<int:pk>", views.ResultDetailView.as_view(), name= "result"),
+path(r"voter/<int:pk>", views.ResultDetailView.as_view(), name= "result"),
 path(r'graphs/', views.GraphsView.as_view(), name='graphs')
 ]
